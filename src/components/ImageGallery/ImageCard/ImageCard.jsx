4c6e8e2a173be730540ffe47bfc55fcb027bd3a7
diana_dobosh <diana.lakatosh.girl@gmail.com>
2025-01-05 11:51:25 +0200

    fix

--- src/components/ImageGallery/ImageCard/ImageCard.jsx
+++ src/components/ImageGallery/ImageCard/ImageCard.jsx
@@ -1,4 +1,4 @@
-import Modal from 'react-modal';
+import ImageModal from '../../ImageModal/ImageModal';
 import s from '../ImageGallery.module.css';
 import { FaMagnifyingGlassPlus } from 'react-icons/fa6';
 
@@ -20,48 +20,12 @@ export default function ImageCard({
       <div className={s['magnifying-glass']}>
         <FaMagnifyingGlassPlus />
       </div>
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        contentLabel="Image Modal"
-        style={{
-          overlay: {
-            backgroundColor: 'rgba(0, 0, 0, 0.9)',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            animation: 'fadeIn 0.3s ease-in-out',
-          },
-          content: {
-            backgroundColor: 'white',
-            inset: '0',
-            position: 'relative',
-            padding: '0',
-            border: 'none',
-            background: 'none',
-            textAlign: 'center',
-            boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
-          },
-        }}
-      >
-        <img
-          src={item.urls.full}
-          alt="Modal Content"
-          width={1125}
-          height={620}
-        />
-        {item.alt_description && (
-          <div className={s.descriptionModal}>
-            <p className={s.modalDescription}>{item.alt_description}</p>
-            <p>{item.user.name}</p>
-          </div>
-        )}
-      </Modal>
+      <ImageModal
+        isModalOpen={isModalOpen}
+        openModal={openModal}
+        item={item}
+        closeModal={closeModal}
+      />
     </div>
   );
 }
---
+++ src/components/ImageModal/ImageModal.jsx
@@ -0,0 +1,50 @@
+import Modal from 'react-modal';
+import s from '../ImageGallery/ImageGallery.module.css';
+export default function ImageModal({ closeModal, isModalOpen, item }) {
+  return (
+    <>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Image Modal"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.9)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            animation: 'fadeIn 0.3s ease-in-out',
+          },
+          content: {
+            backgroundColor: 'white',
+            inset: '0',
+            position: 'relative',
+            padding: '0',
+            border: 'none',
+            background: 'none',
+            textAlign: 'center',
+            boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+          },
+        }}
+      >
+        <img
+          src={item.urls.full}
+          alt="Modal Content"
+          width={1125}
+          height={620}
+        />
+        {item.alt_description && (
+          <div className={s.descriptionModal}>
+            <p className={s.modalDescription}>{item.alt_description}</p>
+            <p>{item.user.name}</p>
+          </div>
+        )}
+      </Modal>
+    </>
+  );
+}
